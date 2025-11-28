2adddaa064b93913720ce24951ba600b4551d6fe
Alex Yu <alex.yu.dev@gmail.com>
2025-11-28 17:23:42 +0200

    chore: remove comments

--- cmd/gob/examples.go
+++ cmd/gob/examples.go
@@ -45,7 +45,6 @@ func LoadGob[T any](filepath string) (T, error) {
 }
 
 func SaveGobz(fileName string, data any) {
-	// file, err := os.OpenFile(fileName, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
 	gobFile, err := os.OpenFile(fileName, os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		fmt.Printf("Error opening file: %v\n", err)
